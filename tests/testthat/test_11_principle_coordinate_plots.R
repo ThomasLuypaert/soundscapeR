170eb8a9d6ffdfc169c9bafdac52a4fb8922c585
ThomasLuypaert <thomas.luypaert@nmbu.no>
2023-03-07 10:39:48 +0100

    Trying to find new way to set seed with testing functions

--- tests/testthat/test_11_principle_coordinate_plots.R
+++ tests/testthat/test_11_principle_coordinate_plots.R
@@ -1,6 +1,16 @@
 library(testthat)
 library(soundscapeR)
 
+# 0. Loading required function
+
+with_seed <- function(seed, code) {
+  code <- substitute(code)
+  orig.seed <- .Random.seed
+  on.exit(.Random.seed <<- orig.seed)
+  set.seed(seed)
+  eval.parent(code)
+}
+
 # 1. Load the merged '.csv' data frame files,
 # binarize the data frame, aggregate the data frame, and make
 #wrong data frame types for testing purposes
@@ -40,59 +50,62 @@ testthat::test_that("the ss_pcoa function provides the correct error when the ag
 
 })
 
+
 # 2.1. When the correct arguments are supplied
 
   # No grouping without scree plot
 
-# testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
-#
-#   set.seed(30874)
-#
-#   vdiffr::expect_doppelganger(
-#     title = "Create ss_pcoa without grouping or screeplot",
-#     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study ),
-#   )
-# })
-#
-#   # No grouping with scree plot
-#
-# testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
-#
-#   set.seed(30874)
-#
-#   vdiffr::expect_doppelganger(
-#     title = "Create ss_pcoa without grouping but with screeplot",
-#     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study,
-#                                screeplot = TRUE),
-#   )
-# })
-#
-#   # Grouping without scree plot
-#
-# testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
-#
-#   set.seed(30874)
-#
-#   vdiffr::expect_doppelganger(
-#     title = "Create ss_pcoa with grouping but without screeplot",
-#     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study,
-#                                screeplot = FALSE,
-#                                grouping = case_study_groups),
-#   )
-# })
-#
-#   # Grouping with scree plot
-#
-# testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
-#
-#   set.seed(30874)
-#
-#   vdiffr::expect_doppelganger(
-#     title = "Create ss_pcoa with grouping and screeplot",
-#     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study,
-#                                screeplot = TRUE,
-#                                grouping = case_study_groups),
-#    )
-# })
-#
-#
+with_seed(seed = 1234, code =
+
+ testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
+
+   vdiffr::expect_doppelganger(
+     title = "Create ss_pcoa without grouping or screeplot",
+     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study ),
+   )
+ })
+
+)
+
+   # No grouping with scree plot
+
+with_seed(seed = 1234, code =
+ testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
+
+   vdiffr::expect_doppelganger(
+     title = "Create ss_pcoa without grouping but with screeplot",
+     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study,
+                                screeplot = TRUE),
+   )
+ })
+)
+
+   # Grouping without scree plot
+
+with_seed(seed = 1234, code =
+ testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
+
+   vdiffr::expect_doppelganger(
+     title = "Create ss_pcoa with grouping but without screeplot",
+     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study,
+                                screeplot = FALSE,
+                                grouping = case_study_groups),
+   )
+ })
+)
+
+   # Grouping with scree plot
+
+with_seed(seed = 1234, code =
+ testthat::test_that("The ss_pcao function works as expected when the correct arguments are supplied", {
+
+   vdiffr::expect_doppelganger(
+     title = "Create ss_pcoa with grouping and screeplot",
+     fig = soundscapeR::ss_pcoa(aggregated_soundscape_list = aggregated_soundscape_CVR_case_study,
+                                screeplot = TRUE,
+                                grouping = case_study_groups),
+    )
+ })
+)
+
+
